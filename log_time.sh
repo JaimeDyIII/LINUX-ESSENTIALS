ead2a6dc25944675d2675506572e9588e83be904
Jaime Dy III <152839721+JaimeDyIII@users.noreply.github.com>
2025-04-02 08:48:16 +0800

    Update log_time.sh

--- log_time.sh
+++ log_time.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #!/bin/bash
-$count=0
-(($count++))
-echo -e "Current Date and Time: $(date)\nUsername: JaimeDyIII\n$(count)" >> log.txt
+count=0
+((count++))
+echo -e "Current Date and Time: $(date)\nUsername: JaimeDyIII\n$count" >> log.txt
