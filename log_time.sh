85a2951b974d7b72cda485fe8ee4a4f6250f7cc9
Jaime Dy III <152839721+JaimeDyIII@users.noreply.github.com>
2025-04-05 07:46:21 +0000

    done?

--- log_time.sh
+++ log_time.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-echo -e "Log: Current Date and Time: $(date)\nUsername: JaimeDyIII\n" >> log.txt
+USERNAME=${GITHUB_ACTOR:-"unknown-user"}
+
+if [ -f log.txt ]; then
+  COUNT=$(grep -c "^Log Entry #" log.txt)
+else
+  COUNT=0
+fi
+
+((COUNT++))
+
+{
+  echo "Log Entry #$COUNT"
+  echo "Date & Time: $(date)"
+  echo "Username: $USERNAME"
+  echo "Total Log Entries: $COUNT"
+  echo ""
+} >> log.txt
